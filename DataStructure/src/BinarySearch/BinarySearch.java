7aa24bea0ebadd9f06590226d8f96987f990441f
collie1010 <asd640426@gmail.com>
2025-06-05 20:52:32 +0800

    Create BinarySearchTest.java

--- DataStructure/src/BinarySearch/BinarySearch.java
+++ DataStructure/src/BinarySearch/BinarySearch.java
@@ -60,32 +60,5 @@ public class BinarySearch {
         return binarySearchRecursive(arr, target, mid + 1, right);
     }
     
-    // 主方法進行測試
-    public static void main(String[] args) {
-        // 測試用的已排序陣列
-        int[] arr = {2, 5, 8, 12, 16, 23, 38, 56, 72, 91};
-        
-        // 測試案例
-        int[] testCases = {23, 2, 91, 50, 16};
-        
-        System.out.println("使用迭代方式的二元搜尋：");
-        for (int target : testCases) {
-            int result = binarySearch(arr, target);
-            if (result != -1) {
-                System.out.printf("數字 %d 在索引 %d 的位置%n", target, result);
-            } else {
-                System.out.printf("數字 %d 不在陣列中%n", target);
-            }
-        }
-        
-        System.out.println("\n使用遞迴方式的二元搜尋：");
-        for (int target : testCases) {
-            int result = binarySearchRecursive(arr, target, 0, arr.length - 1);
-            if (result != -1) {
-                System.out.printf("數字 %d 在索引 %d 的位置%n", target, result);
-            } else {
-                System.out.printf("數字 %d 不在陣列中%n", target);
-            }
-        }
-    }
+    
 }
